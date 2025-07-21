882ee988915c8d8feeba7c8df2267b6024514ff3
chana <chana154590@gmail.com>
2025-07-21 12:09:45 +0300

    plc-mock and start opc

---
+++ packages/opc/server.ts
@@ -0,0 +1,21 @@
+
+import { OPCUAServer } from "node-opcua";
+
+const server = new OPCUAServer({
+  port: 5000,
+  resourcePath: "/UA/MyServer",
+  buildInfo: {
+    productName: "MySampleServer",
+    buildNumber: "1",
+    buildDate: new Date()
+  }
+});
+
+async function startServer() {
+  await server.initialize();
+  await server.start();
+  console.log("Server running on", server.endpoints[0].endpointDescriptions()[0].endpointUrl);
+}
+
+startServer();
+export { server };
