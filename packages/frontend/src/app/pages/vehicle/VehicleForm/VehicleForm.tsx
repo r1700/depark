29f4e2918f6d03d86b8a760ce26ff01a55b7995b
Margalit <tichnut48@8547900.org.il>
2025-07-30 10:17:15 +0300

    userForm, vehicleForm, genericForm components

---
+++ packages/frontend/src/app/pages/vehicle/VehicleForm/VehicleForm.tsx
@@ -0,0 +1,42 @@
+import GenericForm from '../../../../components/forms/Form';
+import { FieldConfig } from '../../../../components/forms/Form';
+import { useAppDispatch, useAppSelector } from '../../../hook';
+import { addVehicle, fetchVehicles, updateVehicle } from '../vehicleThunks';
+import { Vehicle } from '../../../types/Vehicle';
+import { mockUsers } from '../../../types/User';
+
+
+const AddOrEditVehicleForm = ({ onClose, vehicleToEdit=null }
+  : {
+    onClose: () => void;
+    vehicleToEdit?: Partial<Vehicle> | null;
+  }) => {
+
+  const dispatch = useAppDispatch();
+  const users = useAppSelector((state) => state.users.users) || mockUsers
+  const newUsers: string[] = mockUsers.map((user) => `${user.firstName} ${user.lastName}`) || [];
+
+
+  const VehicleFields: FieldConfig<Vehicle>[] = [
+    { name: 'licensePlate', label: 'License Plate', required: true, type: 'text' },
+    { name: 'user', label: 'User', required: true, type: 'select', options: newUsers },
+    { name: 'width', label: 'Width', type: 'number' },
+    { name: 'height', label: 'Height', type: 'number' },
+    { name: 'length', label: 'Length', type: 'number' },
+    { name: 'weight', label: 'Weight', type: 'number' },
+    {name: 'dimensionsSource', label: 'dimensionsSource', type: 'select', 
+     options: ['model_reference' ,'manual_override', 'government_db'],required: true},
+    ]
+  return (
+    <GenericForm<Vehicle>
+      title={vehicleToEdit ? 'Edit Vehicle' : 'Add Vehicle'}
+      fields={VehicleFields}
+      initialState={{ 'user': undefined, 'licensePlate': '','dimensionsSource':'manual_override'}}
+      onSubmit={(data) => vehicleToEdit ? dispatch(updateVehicle(data)) : dispatch(addVehicle(data))}
+      onClose={onClose}
+      entityToEdit={vehicleToEdit}
+    />
+  );
+}
+
+export default AddOrEditVehicleForm;
--- packages/frontend/src/app/store.ts
+++ packages/frontend/src/app/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import parkingReducer from './pages/adminDashBoard/parkingSlice';
+import vehicleReducer from './pages/vehicle/vehicleSlice';
+import userReducer from './pages/user/usersSlice';
 
 export const store = configureStore({
-  reducer: { parking: parkingReducer },
+  reducer: { parking: parkingReducer ,
+    vehicles: vehicleReducer,
+    users: userReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
