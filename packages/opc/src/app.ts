bce6cb9c91ca478456137e644ecfcd19d87f091b
baty <bg0548534398@gmail.com>
2025-07-30 12:18:43 +0300

    finish password

--- packages/opc/src/app.ts
+++ packages/opc/src/app.ts
@@ -1,37 +1,46 @@
 import express, { Request, Response } from 'express';
 import { writeNodeValues, WriteItem } from './opc-client';
+import { it } from 'node:test';
 
 const app = express();
 app.use(express.json());
 
 // POST /plc/write
 function extractNodeId(req: Request, res: Response, next: Function): void {
-  const { param } = req.params; // Extract the URL suffix
+  const paramsArray = Object.values(req.params);
 
-  if (!param) {
-    res.status(400).send("NodeId is missing in the URL"); 
+  console.log(paramsArray);
+
+  if (paramsArray.length < 1) {
+    res.status(400).send("At least one parameter is required");
     return;
   }
-  const nodeId = `ns=1;s=${param}`; // Create the nodeId from the param
-  const value = req.body[param]; // Extract the value from the request body
+  const itemArray = paramsArray.map(param => {
+    const nodeId = `ns=1;s=${param}`; // Create the nodeId from the param   
+    const value = req.body[param]; // Extract the value from the request body
 
-  if (!value) {
-    res.status(400).send("License plate is required"); 
+    if (!value) {
+      return;
+    }
+    return {
+      nodeId: nodeId,
+      value: value
+    };
+  });
+  if (itemArray.length === 0) {
+    res.status(400).send("No valid nodeId found");
     return;
   }
-
-  const item: WriteItem[] = [{
-    nodeId: nodeId,
-    value: value
-  }];
-  req.body.nodeId = item;
+  const items = itemArray.filter(item => item != null); // Filter out any undefined items
+  req.body.nodeId = items; // Assign the array to req.body.nodeId
   next(); // Pass the request to the next handler
 }
 
-app.post('/plc/write/:param', extractNodeId, async (req: Request, res: Response) => {
+async function writeToPlc(req: Request, res: Response, next: Function): Promise<void> {
   const writeItems: WriteItem[] = req.body.nodeId || [];
   if (!Array.isArray(writeItems) || writeItems.length === 0) {
-    return res.status(400).send("No write items provided");
+    res.status(400).send("No write items provided");
+    return;
   }
   try {
     await writeNodeValues(writeItems);
@@ -39,7 +48,12 @@ app.post('/plc/write/:param', extractNodeId, async (req: Request, res: Response)
   } catch (err: any) {
     res.status(500).send(err.message || "Unknown error");
   }
-});
+}
+
+
+app.post('/plc/write/:param/:param2', extractNodeId, writeToPlc);
+app.post('/plc/write/:param', extractNodeId, writeToPlc);
+
 
 app.use('/', (req: Request, res: Response) => {
   res.status(404).send("Not Found");
