354aa593c953fbbf5446446babaab6e3be588c8b
Tzipora-kuperberg <tichnut57@8547900.org.il>
2025-07-31 10:46:43 +0300

    resolve conflicts

--- packages/backend/src/db/db.ts
+++ packages/backend/src/db/db.ts
@@ -7,3 +7,4 @@ dotenv.config();
 export const pool = new Pool({
   connectionString: process.env.SUPABASE_FULL_URL || process.env.DATABASE_URL,
 });
+
--- packages/backend/src/services/vehicleService.ts
+++ packages/backend/src/services/vehicleService.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export async function getVehicleDetailsByDegem(degem: string) {
-  const url = 'https://data.gov.il/api/3/action/datastore_search';
-  const params = {
-    resource_id: '142afde2-6228-49f9-8a29-9b6c3a0cbe40',
-    q: degem,
-    limit: 5,
-  };
+// export async function getVehicleDetailsByDegem(degem: string) {
+//   const url = 'https://data.gov.il/api/3/action/datastore_search';
+//   const params = {
+//     resource_id: '142afde2-6228-49f9-8a29-9b6c3a0cbe40',
+//     q: degem,
+//     limit: 5,
+//   };
 
-  const response = await axios.get(url, { params });
-  const vehicleData = response.data.result.records;
+//   const response: Axios.AxiosXHR<unknown> = await axios.get(url, { params });
+//   const vehicleData = response.data.result.records;
 
-  if (vehicleData.length > 0) {
-    const vehicle = vehicleData[0];
-    return {
-      mishkal_kolel: vehicle.mishkal_kolel || 'no data available',
-      gova: vehicle.gova || 'no data available',
-    };
-  } else {
-    throw new Error('No results found for the model');
-  }
-}
+//   if (vehicleData.length > 0) {
+//     const vehicle = vehicleData[0];
+//     return {
+//       mishkal_kolel: vehicle.mishkal_kolel || 'no data available',
+//       gova: vehicle.gova || 'no data available',
+//     };
+//   } else {
+//     throw new Error('No results found for the model');
+//   }
+// }
