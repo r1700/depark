8262432eb77eca4bb7848c28649445f11c939400
M.Schwartz <tichnut56@8547900.org.il>
2025-07-31 13:55:24 +0300

    orginized the files

--- packages/frontend/src/app/pages/adminDashBoard/parkingDashboard.tsx
+++ packages/frontend/src/app/pages/adminDashBoard/parkingDashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchParkingData, setTabs} from './parkingSlice';
-import DataTable from '../../components/table/table';
+import DataTable from '../../../components/table/table';
 
 import type { AppDispatch } from '../../store'; 
 const ParkingDashboard = () => {
