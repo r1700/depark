c998559973823e1aa0ad154a529f72df88eda735
yw <tichnut16@8547900.org.il>
2025-08-06 11:24:37 +0300

    merge my branch whith main

---
+++ packages/opc/src/backendService.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+const backendUrl = `http://${process.env.SOURCE_URL || "localhost:3001"}/vehicle-lookup`;
+
+// פונקציה לשליחת הנתון ל-backend
+async function sendDataToBackend(nodeId: string, value: any) {
+  try {
+    const response = await axios.post(backendUrl, {
+      nodeId: nodeId,
+      value: value
+    });
+    console.log("Backend response:", response.data);
+  } catch (error) {
+    console.error("Error sending data to backend:", error);
+  }
+}
+
+export { sendDataToBackend };
