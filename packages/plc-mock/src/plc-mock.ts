b23b6d7076ea11dae159305cd0cb4decf590bfa1
yw <tichnut16@8547900.org.il>
2025-08-06 11:32:48 +0300

    merge my branch with main

--- packages/plc-mock/src/plc-mock.ts
+++ packages/plc-mock/src/plc-mock.ts
@@ -1,5 +1,5 @@
 import { OPCUAServer, Variant, DataType, StatusCodes } from "node-opcua";
-// הפעלת שרת OPC-UA המדמה PLC
+// Start an OPC-UA server simulating a PLC
 export async function createPlcOpcServer() {
   const server = new OPCUAServer({
     port: 4080,
@@ -17,91 +17,56 @@ export async function createPlcOpcServer() {
   const device = namespace.addObject({
     organizedBy: addressSpace.rootFolder.objects,
     browseName: "PLC",
-    // nodeId: "ns=1;s=PLC"
   });
   console.log("objectsFolder:", addressSpace.rootFolder.objects.nodeId.toString());
-  // משתנים פנימיים לדימוי מצב
-  // כל אחד מהם יש לו nodeIdמשלו
-  let parkingTypeAvailable = false;
-  let carInParking = false;
-  let vehicleEnteredSuccessfully = false;
-  // let valve1 = false;
-  // let valve2 = false;
-  // let valve3 = false;
-  // let valve4 = false;
-  let car= false;
-  let sensor1 = 0;
-  let sensor2 = 0;
-  // הוספת משתני Outputs
+  // Internal variables to simulate state
+  // Each has its own nodeId
+  let licensePlateEntry = "";
+  let licensePlateExit = "";
+  let parkingSpot = "";
+  // Add Outputs variables
   namespace.addVariable({
-  componentOf: device,
-  browseName: "parkingTypeAvailable",
-  nodeId: "ns=1;s=parkingTypeAvailable",
-  dataType: "Boolean",
-  minimumSamplingInterval: 1000, // ← Required when using get
-  value: {
-    get: () => {
-      const value = true;
-      return new Variant({ dataType: DataType.Boolean, value: parkingTypeAvailable});
+    componentOf: device,
+    browseName: "licensePlateEntry",
+    nodeId: "ns=1;s=licensePlateEntry",
+    dataType: "String",
+    minimumSamplingInterval: 1000,
+    value: {
+      get: () => new Variant({ dataType: DataType.String, value: licensePlateEntry }),
+      set: (variant: Variant) => {
+        licensePlateEntry = variant.value;
+        return StatusCodes.Good;
+      },
     },
-    set: (variant: Variant) => {
-      parkingTypeAvailable = variant.value;
-      return StatusCodes.Good;
-    },
-  },
-});
+  });
   namespace.addVariable({
-  componentOf: device,
-  browseName: "carInParking",
-   nodeId: "ns=1;s=carInParking",
-  dataType: "Boolean",
-  minimumSamplingInterval: 1000, // ← נדרש בעת שימוש ב-get
-  value: {
-    get: () => {
-      const value = true;
-      return new Variant({ dataType: DataType.Boolean, value:carInParking });
+    componentOf: device,
+    browseName: "licensePlateExit",
+    nodeId: "ns=1;s=licensePlateExit",
+    dataType: "String",
+    minimumSamplingInterval: 1000,
+    value: {
+      get: () => new Variant({ dataType: DataType.String, value: licensePlateExit }),
+      set: (variant: Variant) => {
+        licensePlateExit = variant.value;
+        return StatusCodes.Good;
+      },
     },
-    set: (variant: Variant) => {
-      carInParking = variant.value;
-      return StatusCodes.Good;
-    },
-  },
-});
+  });
   namespace.addVariable({
-  componentOf: device,
-  browseName: "vehicleEnteredSuccessfully",
-   nodeId: "ns=1;s=vehicleEnteredSuccessfully",
-  dataType: "Boolean",
-  minimumSamplingInterval: 1000, // ← נדרש בעת שימוש ב-get
-  value: {
-    get: () => {
-      const value = true;
-      return new Variant({ dataType: DataType.Boolean, value:vehicleEnteredSuccessfully });
+    componentOf: device,
+    browseName: "parkingSpot",
+    nodeId: "ns=1;s=parkingSpot",
+    dataType: "String",
+    minimumSamplingInterval: 1000,
+    value: {
+      get: () => new Variant({ dataType: DataType.String, value: parkingSpot }),
+      set: (variant: Variant) => {
+        parkingSpot = variant.value;
+        return StatusCodes.Good;
+      },
     },
-    set: (variant: Variant) => {
-      vehicleEnteredSuccessfully = variant.value;
-      return StatusCodes.Good;
-    },
-  },
-});
-namespace.addVariable({
-  componentOf: device,
-  browseName: "car",
-   nodeId: "ns=1;s=car",
-  dataType: "Boolean",
-  minimumSamplingInterval: 1000, // ← נדרש בעת שימוש ב-get
-  value: {
-    get: () => {
-      const value = true;
-      return new Variant({ dataType: DataType.Boolean, value :car});
-    },
-    set: (variant: Variant) => {
-      car = variant.value;
-      return StatusCodes.Good;
-    },
-  },
-});
+  });
   await server.start();
   console.log(":white_check_mark: OPC-UA server running at:", server.endpoints[0].endpointDescriptions()[0].endpointUrl);
 }
-
