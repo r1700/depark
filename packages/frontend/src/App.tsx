455628e8c87a6a7e9cbf34262cd36d5e0fbb80a5
rivky <r6731700@gmail.com>
2025-08-11 12:48:24 +0300

    add

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useCallback } from "react";
 import LoginPage from "./admin/Pages/loginPage";
+<<<<<<< HEAD
 import Otp from "./mobile/components/otp";
+=======
+import Otp from "./mobile/component/otp";
+>>>>>>> 08fb09e7 (add)
 import HomePage from "./tablet/pages/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
