b64e43cc79c0cfde693f0619a67e33e3cbe25f68
Tzipora-kuperberg <tichnut57@8547900.org.il>
2025-08-11 13:05:01 +0300

    p

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,21 +2,13 @@ import React, { useState, useEffect, useCallback } from "react";
 import LoginPage from "./admin/Pages/loginPage";
 import Otp from "./mobile/components/otp";
 import HomePage from "./tablet/pages/HomePage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/admin" element={<LoginPage />} />
-
-          <Route path="/mobile" element={<Otp />} />
-
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Router>
+      <Outlet />
     </>
 
   );
