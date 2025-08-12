3f43c3fd7bb01bfe3b21556d3eef326f1e6db521
rachel <username@users.noreply.github.com>
2025-08-12 11:33:43 +0300

    move all the files from the query to new branch

--- packages/backend/src/config/sequelize.config.ts
+++ packages/backend/src/config/sequelize.config.ts
@@ -1,20 +1,47 @@
-// config/sequelize.config.ts
-import path from 'path';
-import { appDbConfig } from './config';
+// import path from 'path';
+// import dotenv from 'dotenv';
+// dotenv.config({ path: path.resolve(__dirname, '../../../.env') }); // Load environment variables from the root .env file
 
-export default {
-  development: {
-    ...appDbConfig,
-    migrations: {
-      path: path.resolve(__dirname, '../../migrations'),
-      pattern: /\.(js|ts)$/,
-    },
-  },
-  production: {
-    ...appDbConfig,
-    migrations: {
-      path: path.resolve(__dirname, '../../migrations'),
-      pattern: /\.(js|ts)$/,
-    },
-  },
-};
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME }: any = process.env;
+
+// // const { DATA_USERNAME, PASSWORD, HOST, DATABASE }: any = process.env;
+
+// export default  {
+//   development: {
+//     username: DB_USER ,
+//     password: DB_PASSWORD ,
+//     database: DB_NAME ,
+//     host: DB_HOST ,
+//     dialect: 'postgres',
+//     migrationStorageTableName: 'sequelize_meta',
+//     migrations: {
+//       path: path.resolve(__dirname, '../../../migrations'),
+//       pattern: /\.js|ts$/
+//     }
+//   }
+// };
+
+
+import path from 'path';
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+// const { DATA_USERNAME, PASSWORD, HOST, DATABASE }: any = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME }: any = process.env;
+
+// console.log(process.env);
+// console.log('Password:', PASSWORD);
+export const sequelize = new Sequelize(
+  DB_NAME || '',
+  DB_USER || 'postgres',
+  DB_PASSWORD || '1234',
+  {
+    host: DB_HOST || 'localhost',
+    dialect: 'postgres',
+    // migrationStorageTableName: 'sequelize_meta',
+    // migrations: {
+    //   path: path.resolve(__dirname, '../../../migrations'),
+    //   pattern: /\.js|ts$/
+    // }
+  }
+);
