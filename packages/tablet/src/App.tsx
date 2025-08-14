fb3fbc6b3ad0c92d4fbc7ed435aafd2a5b8b43ee
sari <tichnut49@8547900.org.il>
2025-08-14 09:57:37 +0300

    before connect pg

--- packages/tablet/src/App.tsx
+++ packages/tablet/src/App.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UnifiedEntry from './components/UnifiedEntry/UnifiedEntry';
+import VehicleQueue from './components/VehicleQueue/VehicleQueue';
+import { sampleVehicles } from './data';
 
 const App: FC = () => {
         return (
-            <UnifiedEntry />
+            <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UnifiedEntry />} />
+        <Route path="/VehicleQueue" element={<VehicleQueue/>} />
+      </Routes>
+    </BrowserRouter>
     );
 }
 export default App;
-
