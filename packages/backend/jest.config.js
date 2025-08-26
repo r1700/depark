bfe889526562bdbf05bd302abcf1269d0ad84f5d
chana <chana154590@gmail.com>
2025-08-26 14:38:06 +0300

    config to git add

---
+++ packages/backend/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+};
