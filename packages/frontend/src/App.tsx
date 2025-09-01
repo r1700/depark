2fc34fb86aab2d751190f05bf9f907c99e855a0b
henny <tichnut32@8547900.org.il>
2025-09-01 12:56:34 +0300

    final fix_Variable

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+  //  import React, { useState, useCallback } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AdminConfigPage from "./admin/components/AdminConfigPage";
 import ParkingsPage from "./admin/Pages/ParkingsPage";
@@ -9,7 +9,7 @@ import ForgotPassword from './admin/app/pages/resetPassword/ForgotPassword'
 import ResetPassword from './admin/app/pages/resetPassword/ResetPassword';
 import AdminRoutes from "./admin/AdminRoutes";
 
-const user = JSON.parse(localStorage.getItem("user") || '{}');
+  // const user = JSON.parse(localStorage.getItem("user") || '{}');
 const App: React.FC = () => {
 
   return (
