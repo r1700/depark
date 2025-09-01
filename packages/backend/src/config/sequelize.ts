15f463fde68220d66db80e32ee3cf61578c646da
Tzipora-kuperberg <tichnut57@8547900.org.il>
2025-09-01 14:48:55 +0300

    p

--- packages/backend/src/config/sequelize.ts
+++ packages/backend/src/config/sequelize.ts
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   appDbConfig.password,
   {
     host: appDbConfig.host,
-    dialect: appDbConfig.dialect,
+    dialect: process.env.STATUS === 'development' ? appDbConfig.dialect : undefined,
     logging: process.env.STATUS === 'development' ? console.log : false,
   }
 );
