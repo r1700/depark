7240a2386e99672d699c024c1b52af18af6852c2
sari <tichnut49@8547900.org.il>
2025-09-03 11:39:08 +0300

    everithing is running

--- packages/backend/src/config/sequelize.ts
+++ packages/backend/src/config/sequelize.ts
@@ -1,16 +1,13 @@
-// config/sequelize.ts
 import { Sequelize } from 'sequelize';
 import { appDbConfig } from './config';
-
 const sequelize = new Sequelize(
   appDbConfig.database,
   appDbConfig.username,
   appDbConfig.password,
   {
     host: appDbConfig.host,
-    dialect: process.env.STATUS === 'development' ? appDbConfig.dialect : undefined,
+    dialect: appDbConfig.dialect,
     logging: process.env.STATUS === 'development' ? console.log : false,
   }
 );
-
 export default sequelize;
