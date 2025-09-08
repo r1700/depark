250bf28207ae30af9ae33d2505d8ea87d66b2b1a
rivksh <rw3199127@gmail.com>
2025-09-08 15:25:39 +0300

    fix conflict

--- packages/backend/src/models/UserSession.ts
+++ packages/backend/src/models/UserSession.ts
@@ -1,5 +1,6 @@
 import { Model, DataTypes, Optional } from 'sequelize';
-import sequelize from '../config/database';
+import sequelize from '../config/sequelize';
+
 
 export interface UserSessionAttributes {
   id: number;
